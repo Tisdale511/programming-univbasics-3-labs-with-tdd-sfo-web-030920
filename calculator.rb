315184ecd61bb96af4aa96f1f021bed354b8fa62
Tisdale Fry <tisdalefry@gmail.com>
2020-02-21 03:54:56 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,10 +1,6 @@
-def calculator
 first_number = 0
 second_number = 1
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
 quotient = first_number / second_number
-end
-
-calculator
